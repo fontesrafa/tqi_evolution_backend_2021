f87695bf2944e4a49e2a82d3e36e98867b94486c
fontesrafa <rafaelfontes777@hotmail.com>
2021-12-28 18:55:09 -0300

    Initial commit - created project

---
+++ src/test/java/br/com/tqi/creditanalysis/CreditanalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tqi.creditanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditanalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
