6d3729c2ba84c88350ffee6f7a684bb18477a39d
fontesrafa <rafaelfontes777@hotmail.com>
2021-12-29 12:18:46 -0300

    Added Lombok dependency and updated class with lombok annotations

--- src/main/java/br/com/tqi/creditanalysis/entities/Address.java
+++ src/main/java/br/com/tqi/creditanalysis/entities/Address.java
@@ -1,7 +1,11 @@
 package br.com.tqi.creditanalysis.entities;
 
-import java.util.Objects;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
     
     private Long id;
@@ -17,139 +21,5 @@ public class Address {
     private String state;
 
     private String country;
-
-
-    public Address() {
-    }
-
-    public Address(Long id, String street, Long number, Long postalCode, String city, String state, String country) {
-        this.id = id;
-        this.street = street;
-        this.number = number;
-        this.postalCode = postalCode;
-        this.city = city;
-        this.state = state;
-        this.country = country;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getStreet() {
-        return this.street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public Long getNumber() {
-        return this.number;
-    }
-
-    public void setNumber(Long number) {
-        this.number = number;
-    }
-
-    public Long getPostalCode() {
-        return this.postalCode;
-    }
-
-    public void setPostalCode(Long postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public String getCity() {
-        return this.city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return this.state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getCountry() {
-        return this.country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public Address id(Long id) {
-        setId(id);
-        return this;
-    }
-
-    public Address street(String street) {
-        setStreet(street);
-        return this;
-    }
-
-    public Address number(Long number) {
-        setNumber(number);
-        return this;
-    }
-
-    public Address postalCode(Long postalCode) {
-        setPostalCode(postalCode);
-        return this;
-    }
-
-    public Address city(String city) {
-        setCity(city);
-        return this;
-    }
-
-    public Address state(String state) {
-        setState(state);
-        return this;
-    }
-
-    public Address country(String country) {
-        setCountry(country);
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof Address)) {
-            return false;
-        }
-        Address address = (Address) o;
-        return Objects.equals(id, address.id) && Objects.equals(street, address.street) && Objects.equals(number, address.number) && Objects.equals(postalCode, address.postalCode) && Objects.equals(city, address.city) && Objects.equals(state, address.state) && Objects.equals(country, address.country);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, street, number, postalCode, city, state, country);
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-            " id='" + getId() + "'" +
-            ", street='" + getStreet() + "'" +
-            ", number='" + getNumber() + "'" +
-            ", postalCode='" + getPostalCode() + "'" +
-            ", city='" + getCity() + "'" +
-            ", state='" + getState() + "'" +
-            ", country='" + getCountry() + "'" +
-            "}";
-    }
 
 }
