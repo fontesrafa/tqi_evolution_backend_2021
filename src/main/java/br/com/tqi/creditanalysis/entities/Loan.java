546def61a388655188b12caf05281367603764c6
fontesrafa <rafaelfontes777@hotmail.com>
2022-01-02 11:27:28 -0300

    Added status to Loan Entity class

--- src/main/java/br/com/tqi/creditanalysis/entities/Loan.java
+++ src/main/java/br/com/tqi/creditanalysis/entities/Loan.java
@@ -1,9 +1,9 @@
 package br.com.tqi.creditanalysis.entities;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -28,13 +28,15 @@ public class Loan implements Serializable {
 
     private Double amount;
 
-    private LocalDate firstPaymentDate;
+    private String status;
+
+    private String firstPaymentDate;
 
     private Long installmentsAmount;
 
     @OneToOne
     private Client client;
     
-    @OneToMany
+    @OneToMany(cascade=CascadeType.ALL)
     private List<Installment> installments;
 }
